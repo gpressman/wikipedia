af0d31dbd76af304569250c3970bbe32af895b58
Gabe Pressman <pressman4747@gmail.com>
2016-04-12 19:30:00 -0400

    now pulls data from wiki api

--- js.js
+++ js.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
+	criteria = $("criteia").text()
+
 	$("#random").click(function(){
 		window.open("http://en.wikipedia.org/wiki/Special:Random");
+	})
+    
+	$("#search").click(function(){
+		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles="+ criteria + "Main%20Page&prop=revisions&rvprop=content&format=json&callback=?", function(json){
+			result = JSON.stringify(json);
+			$("#result").html(result)
+		})
+
 	})
 })
