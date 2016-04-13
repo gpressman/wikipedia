323a3ebb218b1b82b53fac22c4ae19caf2a4d17e
Gabe Pressman <pressman4747@gmail.com>
2016-04-13 16:40:04 -0400

    links now work properly

--- js.js
+++ js.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
-	
-	$("#random").click(function(){
-		window.open("http://en.wikipedia.org/wiki/Special:Random");
-	})
-    
+	  
 	$("#search").click(function(){
 	criteria = $("#criteria").val();	$.getJSON("http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=15&search="+ criteria + "&prop=revisions&rvprop=content&format=json&callback=?", function(json){
 			results = ""
 			for (i = 0; i < 15; i++){
+              link = JSON.stringify(json[3][i]).replace(/"/g, "")
+              title = JSON.stringify(json[1][i]).replace(/"/g, "")
               results += "<div class='article'>"  
-              results += "<h2>" + "<a href='" +JSON.stringify(json[3][i]) + "'>" + JSON.stringify(json[1][i]) + "</a>" +"</h2>"
+              results += "<h2 text-align:center>" + "<a target='blank' href='" +link + "'>" + title + "</a>" +"</h2>"
               results += JSON.stringify(json[2][i])
               results +=  "</div>"
 			}
