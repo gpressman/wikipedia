a24fbd6c0b3de318cfe4813d715fd22fae264088
Gabe Pressman <pressman4747@gmail.com>
2016-04-12 23:36:28 -0400

    now displaying proper data

--- js.js
+++ js.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
 	
-
 	$("#random").click(function(){
 		window.open("http://en.wikipedia.org/wiki/Special:Random");
 	})
     
 	$("#search").click(function(){
-		criteria = $("criteria").val()
-		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles="+ criteria + "&prop=revisions&rvprop=content&format=json&callback=?", function(json){
+	criteria = $("#criteria").val();	$.getJSON("http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=15&search="+ criteria + "&prop=revisions&rvprop=content&format=json&callback=?", function(json){
 			result = JSON.stringify(json);
 			$("#result").html(result)
 		})
