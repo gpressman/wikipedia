ce83e3a7302610ff5ee948e0e24011eed0a15ecf
Gabe Pressman <pressman4747@gmail.com>
2016-04-13 13:01:11 -0400

    now displays proper link and data below

--- js.js
+++ js.js
@@ -8,8 +8,10 @@ $(document).ready(function() {
 	criteria = $("#criteria").val();	$.getJSON("http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=15&search="+ criteria + "&prop=revisions&rvprop=content&format=json&callback=?", function(json){
 			results = ""
 			for (i = 0; i < 15; i++){
-              results += "<div>" + JSON.stringify(json[1][i]);
-              results += JSON.stringify(json[2][i]) + "</div>"
+              results += "<div class='article'>"  
+              results += "<h2>" + "<a href='" +JSON.stringify(json[3][i]) + "'>" + JSON.stringify(json[1][i]) + "</a>" +"</h2>"
+              results += JSON.stringify(json[2][i])
+              results +=  "</div>"
 			}
 			$("#result").html(results)
 		})
