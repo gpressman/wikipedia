7a895b05b73428eca7e8d71f9851933794751582
Gabe Pressman <pressman4747@gmail.com>
2016-04-13 00:21:05 -0400

    now is formatted nicely with divs

--- js.js
+++ js.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
 	criteria = $("#criteria").val();	$.getJSON("http://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=15&search="+ criteria + "&prop=revisions&rvprop=content&format=json&callback=?", function(json){
 			results = ""
 			for (i = 0; i < 15; i++){
-              results += JSON.stringify(json[1][i]);
-              results += JSON.stringify(json[2][i])
+              results += "<div>" + JSON.stringify(json[1][i]);
+              results += JSON.stringify(json[2][i]) + "</div>"
 			}
 			$("#result").html(results)
 		})
