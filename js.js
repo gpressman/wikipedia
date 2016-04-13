4400762f5cb26ec96da359e7e9ac44f6ff7d9b72
Gabe Pressman <pressman4747@gmail.com>
2016-04-12 21:24:50 -0400

    now I get the block of json I wanted

--- js.js
+++ js.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-	criteria = $("criteia").text()
+	
 
 	$("#random").click(function(){
 		window.open("http://en.wikipedia.org/wiki/Special:Random");
 	})
     
 	$("#search").click(function(){
+		criteria = $("criteria").val()
 		$.getJSON("https://en.wikipedia.org/w/api.php?action=query&titles="+ criteria + "&prop=revisions&rvprop=content&format=json&callback=?", function(json){
 			result = JSON.stringify(json);
 			$("#result").html(result)
